3de63b44cbaa13451bf2ac71d0d6eaec82b6f8ab
nhat-thuan.hoang@ant-tech.eu <nhat-thuan.hoang@ant-tech.eu>
2019-12-19 09:07:45 +0700

    refactor style

--- src/Editor/EditorStyles.js
+++ src/Editor/EditorStyles.js
@@ -1,41 +1,8 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    borderColor: "green",
-    borderWidth: 1,
-    width: 300
-  },
-  textContainer: {
-    alignSelf: "stretch",
-    position: "relative",
-    minHeight: 40,
-    maxHeight: 140
-  },
   input: {
-    fontSize: 16,
-    color: "#000",
-    fontWeight: "400",
-    paddingHorizontal: 20,
-    minHeight: 40,
-    position: "absolute",
-    top: 0,
-    color: "transparent",
-    alignSelf: "stretch",
-    width: "100%"
-  },
-  formmatedTextWrapper: {
-    minHeight: 40,
-    position: "absolute",
-    top: 0,
-    paddingHorizontal: 20,
-    paddingVertical: 5,
-    width: "100%"
-  },
-  formmatedText: {
-    fontSize: 16,
-    fontWeight: "400"
+    flex: 1
   },
   mention: {
     fontSize: 16,
@@ -43,8 +10,15 @@ export default StyleSheet.create({
     backgroundColor: "rgba(36, 77, 201, 0.05)",
     color: "#244dc9"
   },
-  placeholderText: {
-    color: "rgba(0, 0, 0, 0.1)",
-    fontSize: 16
+  leftIcon: {
+    width: 30,
+    height: 30,
+    marginHorizontal: 10
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    maxHeight: 120,
+    alignItems: 'center',
+    padding: 10
   }
 });
